63f13b577a7577775690550bd2e258d1e70dfbef
LDJ <ldj8200@naver.com>
2022-08-23 17:30:29 +0900

    page3 update(0823_end) 지도 업데이트 중앙 버튼 누르면 마커 활성화 앞, 뒤 버튼 누르면 TextView 변경

--- app/src/main/java/com/weseekapp/Page3Activity.java
+++ app/src/main/java/com/weseekapp/Page3Activity.java
@@ -1,21 +1,16 @@
 package com.weseekapp;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.android.volley.Request;
@@ -41,6 +36,10 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
 
     private GoogleMap mMap;
     private MapView mapView;
+    private TextView tv_name;
+    private ImageView btn_pre, btn_next;
+    private int cnt = 0;
+
     final String URL = "https://dokkydokky.herokuapp.com/getAllData";
     RequestQueue requestQueue;
 
@@ -52,31 +51,35 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
     LatLng korea = new LatLng(36.4894573, 127.7294827);
     LatLng gwangju = new LatLng(35.1398252, 126.8109661);
 
-    private Button btn_array, btn_marker, btn_location;
+    private Button btn_current, btn_location;
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.page3, container, false);
 
-//        if (requestQueue == null) {
-//            requestQueue = Volley.newRequestQueue(view.getContext());
-//        }
-//
-//        btn_array = view.findViewById(R.id.btn_array);
-//        btn_marker = view.findViewById(R.id.btn_marker);
-//
-//        //btn_location = view.findViewById(R.id.btn_location);
-//
-//        btn_array.setOnClickListener(this);
-//        btn_marker.setOnClickListener(this);
-//        //btn_location.setOnClickListener(this);
-//
-//
-//        mapView = view.findViewById(R.id.map);
-//        mapView.onCreate(savedInstanceState);
-//        mapView.getMapAsync(this);
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(view.getContext());
+        }
 
+        tv_name = view.findViewById(R.id.tv_name);
+        btn_current = view.findViewById(R.id.btn_current);
+        //btn_location = view.findViewById(R.id.btn_location);
+        btn_pre = view.findViewById(R.id.btn_pre);
+        btn_next = view.findViewById(R.id.btn_next);
+
+        btn_current.setOnClickListener(this);
+        //btn_location.setOnClickListener(this);
+        btn_pre.setOnClickListener(this);
+        btn_next.setOnClickListener(this);
+
+        mapView = view.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+
+        dbSet thread1 = new dbSet();
+        thread1.start();
+
+        mapView.getMapAsync(this);
 
         return view;
     }
@@ -85,8 +88,6 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
-
-        Log.d("응답", "" + korea);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(korea, 7));
 
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
@@ -100,121 +101,129 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
 
     }
 
+    public class dbSet extends Thread{
+        @Override
+        public void run() {
+            StringRequest request = new StringRequest(
+                    Request.Method.GET,
+                    URL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // 응답 성공 시
+                            Log.d("응답성공", response);
+                            String result = "";
+                            try {
+                                JSONArray jsonArray = new JSONArray(response);
+
+                                name = new String[jsonArray.length()];
+                                location = new String[jsonArray.length()];
+                                gps = new String[jsonArray.length()];
+
+                                for (int i = 0; i < jsonArray.length(); i++) {
+
+                                    JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+
+                                    name[i] = jsonObject.getString("업소명");
+                                    location[i] = jsonObject.getString("소재지");
+                                    gps[i] = jsonObject.getString("GPS");
+
+                                }
+
+                                Log.d("응답성공", "DB 로드 성공!");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // 응답 실패 시
+                            Log.d("응답", "응답 실패");
+                        }
+                    }
+            );
+
+            requestQueue.add(request);
+        }
+    }
+
     @Override
     public void onClick(View view) {
-//        if (view.getId() == R.id.btn_array) {
-//            StringRequest request = new StringRequest(
-//                    Request.Method.GET,
-//                    URL,
-//                    new Response.Listener<String>() {
-//                        @Override
-//                        public void onResponse(String response) {
-//                            // 응답 성공 시
-//                            Log.d("응답성공", response);
-//                            String result = "";
-//                            try {
-//                                JSONArray jsonArray = new JSONArray(response);
-//
-//                                name = new String[jsonArray.length()];
-//                                location = new String[jsonArray.length()];
-//                                gps = new String[jsonArray.length()];
-//
-//                                for (int i = 0; i < jsonArray.length(); i++) {
-//
-//                                    JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-//
-//                                    name[i] = jsonObject.getString("업소명");
-//                                    location[i] = jsonObject.getString("소재지");
-//                                    gps[i] = jsonObject.getString("GPS");
-//
-//
-//
-//
-//
-//                                }
-//
-//
-//                                Log.d("응답성공", "DB 로드 성공!");
-//                            } catch (JSONException e) {
-//                                e.printStackTrace();
-//                            }
-//
-//                        }
-//                    },
-//                    new Response.ErrorListener() {
-//                        @Override
-//                        public void onErrorResponse(VolleyError error) {
-//                            // 응답 실패 시
-//                            Log.d("응답", "응답 실패");
-//                        }
-//                    }
-//            );
-//
-//
-//            requestQueue.add(request);
-//
-//        } else if (view.getId() == R.id.btn_marker) {
-//            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gwangju, 15));
-//            Log.d("응답성공", "마커 표시 성공!");
-//
-//            for (int i = 0; i < name.length; i++) {
-//                gpsS = gps[i].split(",", 2);
-//                float a1 = Float.parseFloat(gpsS[0]);
-//                float a2 = Float.parseFloat(gpsS[1]);
-//                MarkerOptions markerOptions = new MarkerOptions();
-//
-//                markerOptions
-//                        .position(new LatLng(a1, a2))
-//                        .title(name[i])
-//                        .snippet(location[i]);
-//
-//                mMap.addMarker(markerOptions);
+
+        if (view.getId() == R.id.btn_current) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gwangju, 15)); // 차후 현재위치로 변경필요
+            Log.d("응답성공", "마커 표시 성공!");
+
+            for (int i = 0; i < name.length; i++) {
+                gpsS = gps[i].split(",", 2);
+                float a1 = Float.parseFloat(gpsS[0]);
+                float a2 = Float.parseFloat(gpsS[1]);
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                markerOptions
+                        .position(new LatLng(a1, a2))
+                        .title(name[i])
+                        .snippet(location[i]);
+
+                mMap.addMarker(markerOptions);
+
+            }
+        }else if (view.getId() == R.id.btn_pre){
+            tv_name.setText(name[cnt]);
+            cnt--;
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gwangju, 15));
+        }else if (view.getId() == R.id.btn_next){
+            tv_name.setText(name[cnt]);
+            cnt++;
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gwangju, 15));
+        }
+//        } else if (view.getId() == R.id.btn_location) {
+//            LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+//            if (ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return;
 //            }
+//            Location loc_Current = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//            double cur_lat = loc_Current.getLatitude();
+//            double cur_lon = loc_Current.getLongitude();
+//            LatLng current = new LatLng(cur_lat, cur_lon);
+//            Log.d("응답성공", " " + cur_lat + " " + cur_lon);
+//            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 10));
+//
 //        }
-////        } else if (view.getId() == R.id.btn_location) {
-////            LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-////            if (ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-////                // TODO: Consider calling
-////                //    ActivityCompat#requestPermissions
-////                // here to request the missing permissions, and then overriding
-////                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-////                //                                          int[] grantResults)
-////                // to handle the case where the user grants the permission. See the documentation
-////                // for ActivityCompat#requestPermissions for more details.
-////                return;
-////            }
-////            Location loc_Current = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-////            double cur_lat = loc_Current.getLatitude();
-////            double cur_lon = loc_Current.getLongitude();
-////            LatLng current = new LatLng(cur_lat, cur_lon);
-////            Log.d("응답성공", " " + cur_lat + " " + cur_lon);
-////            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 10));
-////
-////        }
 
     }
 
-//    @Override
-//    public void onResume() {
-//        mapView.onResume();
-//        super.onResume();
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        mapView.onPause();
-//    }
-//
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        mapView.onDestroy();
-//    }
-//
-//    @Override
-//    public void onLowMemory() {
-//        super.onLowMemory();
-//        mapView.onLowMemory();
-//    }
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
