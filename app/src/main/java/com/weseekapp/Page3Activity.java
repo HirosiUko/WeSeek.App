a63c9f33886f07932b3f12df5d77263a372462e3
LDJ <ldj8200@naver.com>
2022-08-29 15:14:02 +0900

    Page3 20220829 현재위치와 목적지간 거리 표시 기능 제작 레이아웃 수정필요

--- app/src/main/java/com/weseekapp/Page3Activity.java
+++ app/src/main/java/com/weseekapp/Page3Activity.java
@@ -47,9 +47,12 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
 
     private GoogleMap mMap;
     private MapView mapView;
-    private TextView tv_name, tv_adr, tv_star;
+    private TextView tv_name, tv_adr, tv_star, tv_distance;
     private ImageView btn_pre, btn_next;
     private int cnt = 0;
+
+    private Button btn_search;
+
 
     String url;
     RequestQueue requestQueue;
@@ -64,8 +67,7 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
     Double curLat, curLon;
 
     LatLng currentLoc;
-
-    private Button btn_search;
+    String distance;
 
     ProgressDialog customProgressDialog;
 
@@ -84,6 +86,7 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
         tv_name = view.findViewById(R.id.tv_name);
         tv_adr = view.findViewById(R.id.tv_adr);
         tv_star = view.findViewById(R.id.tv_star);
+        tv_distance = view.findViewById(R.id.tv_distance);
 
         btn_search = view.findViewById(R.id.btn_search);
         btn_pre = view.findViewById(R.id.btn_pre);
@@ -97,24 +100,50 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
         Log.d("에러", "onCreateView: ");
         mapView.onCreate(savedInstanceState);
 
-        dbSet dbthread = new dbSet();
-        dbthread.start();
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(getContext().LOCATION_SERVICE);
 
-        mapView.getMapAsync(this);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        }
+        Location loc_Current = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        curLat = loc_Current.getLatitude();
+        curLon = loc_Current.getLongitude();
+        Log.d("현재위치", "" + curLat + "," + curLon);
+        
+        dbSet dbthread = new dbSet();
+        dbthread.start(); // db 호출
+
+        mapView.getMapAsync(this); // onMapReady 호출
 
         return view;
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) { // 지도 띄우기 + 현재위치 받아와서 마커 표시
 
-        customProgressDialog.show();
+//        customProgressDialog.show();
+
         mMap = googleMap;
 
         currentLoc = new LatLng(curLat, curLon);
+
+        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.markerblue);
+        Bitmap b = bitmapdraw.getBitmap();
+        Bitmap blueMarker = Bitmap.createScaledBitmap(b, 80, 120, false);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(currentLoc)
+                     .title("현재위치")
+                     .icon(BitmapDescriptorFactory.fromBitmap(blueMarker));
+
+        mMap.addMarker(markerOptions);
+
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 17));
 
         mMap.setOnMarkerClickListener(this);
+
+//        customProgressDialog.dismiss();
+
 
 
     }
@@ -131,33 +160,31 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
     public boolean onMarkerClick(@NonNull Marker marker) {
         mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
 
+//        String title = marker.getTitle();
+//        String snippet = marker.getSnippet();
+//        double lat = marker.getPosition().latitude;
+//        double lon = marker.getPosition().longitude;
+//        Log.d("좌표", lat + ", " + lon);
+//        marker.remove();
+//        addMarker(title, snippet, lat, lon, true);
+
+        cnt = Integer.parseInt(marker.getId().replaceAll("[^\\d]", ""))-1;
+        Log.d("태그", ""+cnt);
+
+        getDistance(loc[cnt].latitude, loc[cnt].longitude);
+        infoset(cnt);
+
         tv_name.setText(marker.getTitle());
         tv_adr.setText(marker.getSnippet());
-
-        String title = marker.getTitle();
-        String snippet = marker.getSnippet();
-        double lat = marker.getPosition().latitude;
-        double lon = marker.getPosition().longitude;
-        Log.d("좌표", lat + ", " + lon);
-
-        marker.remove();
-        addMarker(title, snippet, lat, lon, true);
+        tv_distance.setText(distance);
 
         return false;
     }
 
 
-    private class dbSet extends Thread {
+    private class dbSet extends Thread { // db에서 데이터 가져오기
         @Override
         public void run() {
-            LocationManager locationManager = (LocationManager) getActivity().getSystemService(getContext().LOCATION_SERVICE);
-
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            Location loc_Current = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            curLat = loc_Current.getLatitude();
-            curLon = loc_Current.getLongitude();
 
             StringRequest request = new StringRequest(
                     Request.Method.GET,
@@ -211,47 +238,66 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
 
             Log.d("에러", url);
             requestQueue.add(request);
-            customProgressDialog.dismiss();
         }
+    }
+
+    public String getDistance(double lat, double lon){
+        Location locationA = new Location("current");
+        locationA.setLatitude(curLat);
+        locationA.setLongitude(curLon);
+
+        Location locationB = new Location("target");
+        locationB.setLatitude(lat);
+        locationB.setLongitude(lon);
+
+        double dis = locationA.distanceTo(locationB);
+
+        if (dis < 1000){
+            distance = "" + (int)dis + " m";
+        } else{
+            distance = "" + ((int)dis)/10*10 / 1000.0 + " km";
+        }
+        Log.d("거리", distance);
+
+        return distance;
     }
 
     public void infoset(int cnt){
         tv_name.setText(name[cnt]);
         tv_adr.setText(location[cnt]);
         tv_star.setText(star[cnt]);
+        tv_distance.setText(distance);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc[cnt], 17));
     }
 
-    private Marker addMarker(String title, String snippet, double lat, double lon, boolean isSelectedMarker){
-        LatLng position = new LatLng(lat, lon);
+//    private Marker addMarker(String title, String snippet, double lat, double lon, boolean isSelectedMarker){
+//        LatLng position = new LatLng(lat, lon);
+//
+//        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.markerblue);
+//        Bitmap b = bitmapdraw.getBitmap();
+//        Bitmap blueMarker = Bitmap.createScaledBitmap(b, 80, 120, false);
+//
+//
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions
+//                .position(position)
+//                .title(title)
+//                .snippet(snippet)
+//                .icon(BitmapDescriptorFactory.fromBitmap(blueMarker));
+//
+//        return mMap.addMarker(markerOptions);
+//
+//    }
 
-        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.markerblue);
-        Bitmap b = bitmapdraw.getBitmap();
-        Bitmap blueMarker = Bitmap.createScaledBitmap(b, 80, 120, false);
 
+    public void onClick(View view) { // 버튼 클릭 이벤트
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions
-                .position(position)
-                .title(title)
-                .snippet(snippet)
-                .icon(BitmapDescriptorFactory.fromBitmap(blueMarker));
-
-        return mMap.addMarker(markerOptions);
-
-    }
-
-
-    public void onClick(View view) {
-
-        if (view.getId() == R.id.btn_search) {
+        if (view.getId() == R.id.btn_search) { // 주변탐색 버튼 클릭시 이벤트
             // 오류뜨면 위치코드 넣어야함
-
-            currentLoc = new LatLng(curLat, curLon);
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 15));
+            
             Log.d("응답성공", "마커 표시 성공!");
 
-            for (int i = 0; i < name.length; i++) {
+            for (int i = 0; i < name.length; i++) { // db에서 받아온 정보로 마커 표시
 
                 MarkerOptions markerOptions = new MarkerOptions();
                 markerOptions
@@ -262,17 +308,21 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
                 mMap.addMarker(markerOptions);
 
             }
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 17));
+            cnt = 0;
 
         }else if (view.getId() == R.id.btn_pre){
             if (cnt > 0){
                 cnt--;
                 Log.d("cnt", ""+cnt);
+                getDistance(loc[cnt].latitude, loc[cnt].longitude);
                 infoset(cnt);
 
 
             }else if (cnt == 0){
                 cnt = name.length -1;
                 Log.d("cnt", ""+cnt);
+                getDistance(loc[cnt].latitude, loc[cnt].longitude);
                 infoset(cnt);
             }
 
@@ -280,11 +330,13 @@ public class Page3Activity extends Fragment implements OnMapReadyCallback, View.
             if (cnt < name.length -1){
                 cnt++;
                 Log.d("cnt", ""+cnt);
+                getDistance(loc[cnt].latitude, loc[cnt].longitude);
                 infoset(cnt);
 
             }else if (cnt == name.length -1){
                 cnt = 0;
                 Log.d("cnt", ""+cnt);
+                getDistance(loc[cnt].latitude, loc[cnt].longitude);
                 infoset(cnt);
             }
 
