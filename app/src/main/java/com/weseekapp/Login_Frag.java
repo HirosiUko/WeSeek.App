07312841b5c2ffa71134dce344b770a9cbaf24cc
hirosi.uko <hirosi.uko@gmail.com>
2022-09-01 16:50:42 +0900

    Login View를 Page4와 연결하기! - Page4에 왔을때, 로그인이 안되어 있으면 Login Page로 감.

---
+++ app/src/main/java/com/weseekapp/Login_Frag.java
@@ -0,0 +1,19 @@
+package com.weseekapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Login_Frag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main_log_in, container, false);
+        return view;
+    }
+}
