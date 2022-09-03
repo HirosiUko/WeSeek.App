1259e4271886ed6df1a1ad780a8786a803cef357
Dongju <ldj8200@naver.com>
2022-09-03 13:04:00 +0900

    권한설정창 이동

--- app/src/main/java/com/weseekapp/FirstMain.java
+++ app/src/main/java/com/weseekapp/FirstMain.java
@@ -20,15 +20,7 @@ public class FirstMain extends AppCompatActivity implements View.OnClickListener
 
     private TextView tv_logIn, tv_signUp;
 
-    private View firstLayout;
 
-    static final int PERMISSIONS_REQUEST = 0x0000001;
-
-    String[] permission_list = { // 필요한 권한은 여기에
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-    };
 
 
     @Override
@@ -37,9 +29,7 @@ public class FirstMain extends AppCompatActivity implements View.OnClickListener
         setContentView(R.layout.activity_first_main);
 
 
-        firstLayout = findViewById(R.id.firstLayout);
 
-        OnCheckPermission();
 
 
         tv_logIn = findViewById(R.id.tv_logIn);
@@ -50,43 +40,7 @@ public class FirstMain extends AppCompatActivity implements View.OnClickListener
 
     }
 
-    private void OnCheckPermission() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
-                ActivityCompat.requestPermissions(this, permission_list, PERMISSIONS_REQUEST);
-            }else{
-                ActivityCompat.requestPermissions(this, permission_list, PERMISSIONS_REQUEST);
-            }
-        }
 
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Snackbar.make(firstLayout, "권한이 설정되었습니다", Snackbar.LENGTH_LONG).show();
-                }else {
-//
-                    Snackbar.make(firstLayout, "앱 사용을 위해서는 권한 설정이 필요합니다.\n설정으로 이동하시겠습니까?", Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            // 스낵바 OK 클릭시 실행작업
-                            startActivity(new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + BuildConfig.APPLICATION_ID)));
-                        }
-                    }).show();
-                }
-
-                break;
-        }
-
-    }
 
 
     @Override
